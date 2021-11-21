838bec9fb3385c30a3ba8188c2124f050c5d0201
Anthony Cook <github@anthonymcook.com>
2021-11-21 08:35:15 -0600

    Extract utility functions

--- lib/_shared.bash
+++ lib/_shared.bash
@@ -2,58 +2,6 @@
 
 set -o nounset
 
-# datatype functions
-array_contains () {
-  local e match="$1"
-  shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
-  return 1
-}
-
-
-
-# UTILITY FUNCTIONS
-
-# usage: realpath <path>
-# attempts to resolve all symlinks until the origin path is discovered
-# circular symlinks will keep it looping forever
-realpath() {
-  p="$1"
-  # loop until the file is no longer a symlink (or doesn't exist)
-  while [[ -h $p ]]; do
-    d="$( cd -P "$( dirname "$p" )" && pwd )"
-    p="$(readlink -e "$p")"
-    # if $p was a relative symlink
-    # we need to resolve it relative to the path where the symlink file was located
-    [[ $p != /* ]] && p="$d/$p"
-  done
-  cd -P "$(dirname "$p")" && pwd
-}
-
-# usage: elapsed <start_time> <end_time>
-# takes two unix timestamps with nanoseconds
-# returns the difference in human-readable format
-elapsed() {
-  started_at=$1
-  ended_at=$2
-
-  if [[ -x $(which bc) ]]; then
-    dt=$(echo "$ended_at - $started_at" | bc)
-    dd=$(echo "$dt/86400" | bc)
-    dt2=$(echo "$dt-86400*$dd" | bc)
-    dh=$(echo "$dt2/3600" | bc)
-    dt3=$(echo "$dt2-3600*$dh" | bc)
-    dm=$(echo "$dt3/60" | bc)
-    ds=$(echo "$dt3-60*$dm" | bc)
-
-    printf " -- time elapsed: %d:%02d:%02d:%02.4f\n" "$dd" "$dh" "$dm" "$ds"
-  else
-    warn "START: $started_at"
-    warn "END: $ended_at"
-  fi
-}
-
-
 # STARTUP
 
 if [[ -z ${_BASH_SHARED_LIB+unset} ]]; then
---
+++ lib/data.bash
@@ -0,0 +1,16 @@
+# DATA
+#
+# Functions for data manipulation and inspection.
+
+# usage: array_contains <needle> <haystack>
+# example: array_contains "entry" "$list"
+# !! BROKEN !!
+array_contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+
+
