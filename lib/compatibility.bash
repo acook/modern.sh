75468622ce4087ba8498b1b48802e552f2377107
Anthony Cook <github@anthonymcook.com>
2022-02-19 00:14:48 -0600

    Improve comments for compatibility helpers

--- lib/compatibility.bash
+++ lib/compatibility.bash
@@ -1,11 +1,11 @@
 # COMPATIBILITY
 #
-# Functions that support running in different environments while retaining functionalit.
+# Shell polyfills to ensure functionality across different environments.
 
 # usage: gfix <command> <opts>
 # example: gfix readlink -m .
-# will try to prefix the command with a g
-# newer versions of macOS have broken the ability to reliably modify the path in subshells
+# will try to prefix the command with a g, as homebrew and other non-native tools providing gnu userspace tools tend to do
+# newer versions of macOS have broken the ability to reliably modify the path in subshells meaning that even if you include the homebrew path it may not find them
 gfix() {
   if command_exists g$1; then
     g$1 "${@:2}"
@@ -14,6 +14,7 @@ gfix() {
   fi
 }
 
+# attempt to use gnu userspace on macOS if available
 if [[ Darwin = $(uname) ]]; then
 
 readlink() {
