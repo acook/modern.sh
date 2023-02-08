ef924ab164217145a9a3e7ad6d4628b80bae957a
Anthony Cook <github@anthonymcook.com>
2023-02-07 21:24:02 -0600

    Split export from variable set
    Also specify first array value explicitly
    
    - https://www.shellcheck.net/wiki/SC2128
    - https://www.shellcheck.net/wiki/SC2155

--- lib/init.bash
+++ lib/init.bash
@@ -7,23 +7,35 @@ set -o nounset
 
 if [[ -z ${_BASH_SHARED_LIB+unset} ]]; then
   declare -a _BASH_SHARED_LIB
-  _BASH_SHARED_LIB=("$(readlink -e "$BASH_SOURCE")")
+  _BASH_SHARED_LIB=("$(readlink -e "${BASH_SOURCE[0]}")")
 else
   return 0
 fi
 
 echo " -- ($(basename "$(dirname "$(readlink -m "${BASH_SOURCE[-1]}")")")/$(basename "${BASH_SOURCE[-1]}") @ $(date "+%Y-%m-%d %T")) : setting up..." >&2
 
-export SCRIPT_SHARED_PATH="$(readlink -e "$BASH_SOURCE")"
-export SCRIPT_SHARED_NAME="$(basename "$SCRIPT_SHARED_PATH")"
-export SCRIPT_SHARED_DIR="$(dirname "$SCRIPT_SHARED_PATH")"
-export SCRIPT_ORIG_PWD="$(pwd -P)"
+SCRIPT_SHARED_PATH="$(readlink -e "${BASH_SOURCE[0]}")"
+SCRIPT_SHARED_NAME="$(basename "$SCRIPT_SHARED_PATH")"
+SCRIPT_SHARED_DIR="$(dirname "$SCRIPT_SHARED_PATH")"
+SCRIPT_ORIG_PWD="$(pwd -P)"
 
-export SCRIPT_MAIN_PATH="$(readlink -e "$0")"
-export SCRIPT_MAIN_NAME="$(basename "$SCRIPT_MAIN_PATH")"
-export SCRIPT_MAIN_DIR="$(dirname "$SCRIPT_MAIN_PATH")"
-export SCRIPT_MAIN_EXE="$(basename "$SCRIPT_MAIN_DIR")/$SCRIPT_MAIN_NAME"
+SCRIPT_MAIN_PATH="$(readlink -e "$0")"
+SCRIPT_MAIN_NAME="$(basename "$SCRIPT_MAIN_PATH")"
+SCRIPT_MAIN_DIR="$(dirname "$SCRIPT_MAIN_PATH")"
+SCRIPT_MAIN_EXE="$(basename "$SCRIPT_MAIN_DIR")/$SCRIPT_MAIN_NAME"
 
-export SCRIPT_CURRENT_PATH=$SCRIPT_SHARED_PATH
+SCRIPT_CURRENT_PATH=$SCRIPT_SHARED_PATH
+
+export SCRIPT_SHARED_PATH
+export SCRIPT_SHARED_NAME
+export SCRIPT_SHARED_DIR
+export SCRIPT_ORIG_PWD
+
+export SCRIPT_MAIN_PATH
+export SCRIPT_MAIN_NAME
+export SCRIPT_MAIN_DIR
+export SCRIPT_MAIN_EXE
+
+export SCRIPT_CURRENT_PATH
 
 _set_scriptcurrent
