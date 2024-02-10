67f91918073b551f5693a9dd51ebc8e4dbb8e942
Anthony Cook <github@anthonymcook.com>
2024-02-10 03:15:48 -0600

    SSH pipe commands - single pipe at a time for now

--- lib/network.bash
+++ lib/network.bash
@@ -17,3 +17,72 @@ rxcp() {
   # shellcheck disable=SC2029
   ssh "$1" "cat $2" > "$3"
 }
+
+# usage: sshpipe_new <host>
+# example: my_pipe="$(sshpipe_new my_host | tail -n 1)"
+# create a named pipe to send bash commands to remote host
+sshpipe_new() { # manage multiple file descriptors
+  local fdi
+  local fdo
+  local remote
+  local in
+  local out
+  local pid
+  fdi=13
+  fdo=14
+  remote="$1"
+  in="$remote.$fdi.in"
+  out="$remote.$fdo.out"
+  pid="$remote.$fdi.pid"
+
+  mkfifo "$in" "$out"
+  ssh -tt "$remote" < "$in" > "$out" &
+  echo "$!" > "$pid"
+  eval "exec $fdi>$in"
+  eval "exec $fdo<$out"
+  echo "$remote.$fdi"
+}
+
+# usage: sshpipe_close <host>
+# example: sshpipe_close my_host
+# close a preexisting sshpipe
+sshpipe_close() {
+  local fdi
+  local fdo
+  local remote
+  local in
+  local out
+  local pid
+  fdi=13
+  fdo=14
+  remote="$1"
+  in="$remote.$fdi.in"
+  out="$remote.$fdo.out"
+  pid="$remote.$fdi.pid"
+
+  eval "exec $fdi>&-"
+  eval "exec $fdo>&-"
+  kill "$(< "$pid")"
+  echo "$pid"
+  rm -v "$in" "$out" "$pid"
+}
+
+# usage: sshpipe_tx <host> [content]
+# example: sshpipe_tx my_host hostname
+sshpipe_tx() { # TODO: make it read from stdin
+  local fd
+  fd=13
+
+  echo "$@" >&"$fd"
+}
+
+# usage: sshpipe_rx <host>
+# example: sshpipe_rx my_host | grep ' line ' | tee errors.txt
+sshpipe_rx() {
+  local fdo
+  fdo=14
+
+  while read -r -t 0.1 -u "$fdo" LINE; do
+    echo "$LINE"
+  done
+}
