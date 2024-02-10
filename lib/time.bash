843bcc640decfc132725621a4397e932b9c74a76
Anthony Cook <github@anthonymcook.com>
2024-02-10 04:19:47 -0600

    Move elapsed function into time module

--- lib/time.bash
+++ lib/time.bash
@@ -17,3 +17,27 @@ ts_file() { date --utc "+%Y-%m-%d-%H-%M-%S"; }
 # unix timestamp with nanoseconds
 # required by elapsed()
 ts_unix() { date "+%s.%N"; }
+
+# usage: elapsed <start_time> <end_time>
+# takes two unix timestamps with nanoseconds
+# returns the difference in human-readable format
+elapsed() {
+  started_at=$1
+  ended_at=$2
+
+  if command_exists bc; then
+    dt=$(echo "$ended_at - $started_at" | bc)
+    dd=$(echo "$dt/86400" | bc)
+    dt2=$(echo "$dt-86400*$dd" | bc)
+    dh=$(echo "$dt2/3600" | bc)
+    dt3=$(echo "$dt2-3600*$dh" | bc)
+    dm=$(echo "$dt3/60" | bc)
+    ds=$(echo "$dt3-60*$dm" | bc)
+
+    printf "time elapsed: %d:%02d:%02d:%02.4f\n" "$dd" "$dh" "$dm" "$ds"
+  else
+    warn "START: $started_at"
+    warn "END: $ended_at"
+  fi
+}
+
--- lib/utility.bash
+++ lib/utility.bash
@@ -1,27 +1,3 @@
 # UTILITY
 #
 # Functions which perform useful but more complex and less essential tasks.
-
-# usage: elapsed <start_time> <end_time>
-# takes two unix timestamps with nanoseconds
-# returns the difference in human-readable format
-elapsed() {
-  started_at=$1
-  ended_at=$2
-
-  if [[ -x $(which bc) ]]; then
-    dt=$(echo "$ended_at - $started_at" | bc)
-    dd=$(echo "$dt/86400" | bc)
-    dt2=$(echo "$dt-86400*$dd" | bc)
-    dh=$(echo "$dt2/3600" | bc)
-    dt3=$(echo "$dt2-3600*$dh" | bc)
-    dm=$(echo "$dt3/60" | bc)
-    ds=$(echo "$dt3-60*$dm" | bc)
-
-    printf " -- time elapsed: %d:%02d:%02d:%02.4f\n" "$dd" "$dh" "$dm" "$ds"
-  else
-    warn "START: $started_at"
-    warn "END: $ended_at"
-  fi
-}
-
