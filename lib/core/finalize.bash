befe2a3a7d8e668baec514833fd784bfcd9bcff0
Anthony Cook <github@anthonymcook.com>
2024-02-10 04:02:37 -0600

    Test for if run from a file and handle if not

--- lib/core/finalize.bash
+++ lib/core/finalize.bash
@@ -3,44 +3,4 @@
 # Finalization of setup.
 # Don't overwrite these values manually.
 
-# directory that execution began in
-# useful if we lose track of where we started after `cd`
-MODERN_SCRIPT_ORIG_PWD="$(pwd -P)"
-
-# get the full path to this file being included (typically ending in "modern.sh")
-MODERN_SCRIPT_FULLPATH="$(readlink -e "${BASH_SOURCE[0]}")"
-# name of this file being included (typically "modern.sh")
-MODERN_SCRIPT_NAME="$(basename "$MODERN_SCRIPT_FULLPATH")"
-# directory that contains this file being included
-# particularly useful if we want to store other libraries relative to it
-MODERN_SCRIPT_DIR="$(dirname "$MODERN_SCRIPT_FULLPATH")"
-
-# get the full path to the script that was originally run
-# this is typically the one that later included modern.sh
-MODERN_MAIN_FULLPATH="$(readlink -e "$0")"
-# name of the file that was originally run
-MODERN_MAIN_NAME="$(basename "$MODERN_MAIN_FULLPATH")"
-# directory that contains the file that was originally run
-MODERN_MAIN_DIR="$(dirname "$MODERN_MAIN_FULLPATH")"
-# reconstruct the full path, should be identical to SCRIPT_MAIN_PATH, but not always
-MODERN_MAIN_EXE="$(basename "$MODERN_MAIN_DIR")/$MODERN_MAIN_NAME"
-
-# initialize the MODERN_CURRENT_FULLPATH variable before it is exported
-# will be overwritten by _set_current_script shortly
-MODERN_CURRENT_FULLPATH=$MODERN_SCRIPT_FULLPATH
-
-export MODERN_SCRIPT_ORIG_PWD
-
-export MODERN_SCRIPT_FULLPATH
-export MODERN_SCRIPT_NAME
-export MODERN_SCRIPT_DIR
-export MODERN_SCRIPT_DIR
-
-export MODERN_MAIN_FULLPATH
-export MODERN_MAIN_NAME
-export MODERN_MAIN_DIR
-export MODERN_MAIN_EXE
-
-export MODERN_CURRENT_FULLPATH
-
 _set_current_script
--- lib/core/preamble.bash
+++ lib/core/preamble.bash
@@ -3,7 +3,41 @@
 # Basic initialization.
 # Don't overwrite these values manually.
 
+# directory that execution began in
+# useful if we lose track of where we started after `cd`
+MODERN_SCRIPT_ORIG_PWD="$(pwd -P)"
+
+# shellcheck disable=SC2128
+if [[ -n $BASH_SOURCE ]]; then
+  # get the full path to this file being included (typically ending in "modern.sh")
+  MODERN_SCRIPT_FULLPATH="$(readlink -e "${BASH_SOURCE[0]}")"
+
+  # get the full path to the script that was originally run
+  # this is typically the one that later included modern.sh
+  MODERN_MAIN_FULLPATH="$(readlink -e "$0")"
+else # the script was run via something like `curl https://example.com/script.bash | bash -s`
+  MODERN_SCRIPT_FULLPATH="$MODERN_SCRIPT_ORIG_PWD/modern.sh"
+  MODERN_MAIN_FULLPATH="$MODERN_SCRIPT_ORIG_PWD/modern.sh"
+fi
+
 set -o nounset
+
+# name of this file being included (typically "modern.sh")
+MODERN_SCRIPT_NAME="$(basename "$MODERN_SCRIPT_FULLPATH")"
+# directory that contains this file being included
+# particularly useful if we want to store other libraries relative to it
+MODERN_SCRIPT_DIR="$(dirname "$MODERN_SCRIPT_FULLPATH")"
+
+# name of the file that was originally run
+MODERN_MAIN_NAME="$(basename "$MODERN_MAIN_FULLPATH")"
+# directory that contains the file that was originally run
+MODERN_MAIN_DIR="$(dirname "$MODERN_MAIN_FULLPATH")"
+# reconstruct the full path, should be identical to SCRIPT_MAIN_PATH, but not always
+MODERN_MAIN_EXE="$(basename "$MODERN_MAIN_DIR")/$MODERN_MAIN_NAME"
+
+# initialize the MODERN_CURRENT_FULLPATH variable before it is exported
+# will be overwritten by _set_current_script during finalize
+MODERN_CURRENT_FULLPATH=$MODERN_SCRIPT_FULLPATH
 
 if [[ -z ${_MODERN_LOADED_LIBS+unset} ]]; then
   declare -a _MODERN_LOADED_LIBS
@@ -45,3 +79,17 @@ fi
 if [[ $MODERN_QUIET != "true" ]]; then
   echo " -- ($(basename "$(dirname "$(readlink -m "${BASH_SOURCE[-1]}")")")/$(basename "${BASH_SOURCE[-1]}") @ $(date "+%Y-%m-%d %T")) : setting up..." >&2
 fi
+
+export MODERN_SCRIPT_ORIG_PWD
+
+export MODERN_SCRIPT_FULLPATH
+export MODERN_SCRIPT_NAME
+export MODERN_SCRIPT_DIR
+export MODERN_SCRIPT_DIR
+
+export MODERN_MAIN_FULLPATH
+export MODERN_MAIN_NAME
+export MODERN_MAIN_DIR
+export MODERN_MAIN_EXE
+
+export MODERN_CURRENT_FULLPATH
