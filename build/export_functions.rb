0979d68a4afebe1ac09eecaef6891ec5538993e2
Anthony Cook <github@anthonymcook.com>
2024-02-15 01:36:13 -0600

    Group the exports directly with the declaration of the functions so they can be wrapped conditionally

--- build/export_functions.rb
+++ build/export_functions.rb
@@ -1,11 +1,21 @@
 #!/usr/bin/env ruby
-
-r = /(\w+)(\(\)\s*\{.*?\})/m
-
-input = $stdin.read
-
-puts input, ""
-
-input.scan(r) do |m|
-  puts "export -f #{m.first}"
+r = /(\s*)(\w+)(\(\)\s*\{)/
+ob = /\{/
+cb = /\}/
+bc = 0
+i = nil
+f = nil
+$stdin.each_line do |line|
+  cbc = line.scan(cb).length
+  bc += line.scan(ob).length - cbc
+  r.match line do |m|
+    i = m.captures[0]
+    f = m.captures[1]
+  end
+  puts line
+  if f && !f.empty? && cbc > 0 && bc == 0
+    puts "#{i}export -f #{f}"
+    i = nil
+    f = nil
+  end
 end
